59cf10d1c887c8be0ce2802723eff26d7c98df40
Mohammed El Alladi <moal@eaaa.dk>
2025-09-20 14:34:33 +0200

    Commit register_test

--- UsersWebApiTest_Module3/ControllerTests.cs
+++ UsersWebApiTest_Module3/ControllerTests.cs
@@ -1,11 +1,45 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
 namespace UsersWebApiTest_Module3
 {
     [TestClass]
-    public class ControllerTests
+    public class UserControllerTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        private AppDbContext GetInMemoryDbContext()
         {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: System.Guid.NewGuid().ToString()) // unique DB per test
+                .Options;
+
+            return new AppDbContext(options);
         }
-    }
+
+        [TestMethod]
+        public void Register_NewUser_ReturnsOk()
+        {
+            // Arrange
+            var context = GetInMemoryDbContext();
+            var controller = new UserController(context);
+
+            var newUser = new UserDto
+            {
+                Username = "testuser",
+                Password = "password123"
+            };
+
+            // Act
+            IActionResult result = controller.Register(newUser);
+
+            // Assert
+            var okResult = result as OkObjectResult;
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual("User registered successfully", okResult.Value);
+
+            // Ensure the user was added to the database
+            Assert.AreEqual(1, context.Users.Count());
+            Assert.AreEqual("testuser", context.Users.First().Username);
+        }
 }
