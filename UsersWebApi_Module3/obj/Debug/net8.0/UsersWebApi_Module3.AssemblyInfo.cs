a72e366b97df6bf2cd8cab99b04e4a2441323a8f
Mohammed El Alladi <moal@eaaa.dk>
2025-09-24 18:21:02 +0200

    commit

--- UsersWebApi_Module3/obj/Debug/net8.0/UsersWebApi_Module3.AssemblyInfo.cs
+++ UsersWebApi_Module3/obj/Debug/net8.0/UsersWebApi_Module3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UsersWebApi_Module3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31da0ef12b5fbf2be615a5c253ab456aeb872a1d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+087f98b5851a6c12086d6611d41c0294c145c86a")]
 [assembly: System.Reflection.AssemblyProductAttribute("UsersWebApi_Module3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UsersWebApi_Module3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
