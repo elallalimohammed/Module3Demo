03ad768bedc0cc4b315ebf9deec8f1118b23d95d
Mohammed El Alladi <moal@eaaa.dk>
2025-09-25 11:27:38 +0200

    commit x

--- UsersWebApiTest_Module3/UserControllerTestsWithMoq.cs
+++ UsersWebApiTest_Module3/UserControllerTestsWithMoq.cs
@@ -11,33 +11,58 @@ namespace UsersWebApiTest_Module3
     public class UserControllerTestsWithMoq
     {
 
-    [TestMethod]
-    public void GetAll_ReturnsOkResult_WithUsers()
-    {
-        // Arrange
-        var mockRepo = new Mock<AuthController.IRepository<User>>();
-        var users = new List<User>
+        [TestMethod]
+        public void GetAll_ReturnsOkResult_WithUsers()
+        {
+            // Arrange
+            var mockRepo = new Mock<AuthController.IRepository<User>>();
+            var users = new List<User>
         {
             new User { Id = 1, Username = "Alice" },
             new User { Id = 2, Username = "Bob" }
         };
 
-        mockRepo.Setup(r => r.GetAll()).Returns(users);
+            mockRepo.Setup(r => r.GetAll()).Returns(users);
 
-        var controller = new AuthController(mockRepo.Object);
+            var controller = new AuthController(mockRepo.Object);
 
-        // Act
-        var result = controller.GetAll().Result; // since GetAll is async
-        var okResult = result as OkObjectResult;
+            // Act
+            var result = controller.GetAll().Result; // since GetAll is async
+            var okResult = result as OkObjectResult;
 
-        // Assert
-        Assert.IsNotNull(okResult);
-        Assert.AreEqual(200, okResult.StatusCode);
+            // Assert
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(200, okResult.StatusCode);
 
-        var returnedUsers = okResult.Value as IEnumerable<User>;
-        Assert.IsNotNull(returnedUsers);
-        Assert.AreEqual(2, returnedUsers.Count());
-       Assert.AreEqual("Alice", returnedUsers.First().Username);
-    }
+            var returnedUsers = okResult.Value as IEnumerable<User>;
+            Assert.IsNotNull(returnedUsers);
+            Assert.AreEqual(2, returnedUsers.Count());
+            Assert.AreEqual("Alice", returnedUsers.First().Username);
+        }
+
+
+        [TestMethod]
+public void GetAll_ReturnsOkResult_WithEmptyList_WhenNoUsersExist()
+{
+    // Arrange
+    var mockRepo = new Mock<AuthController.IRepository<User>>();
+    var emptyUsers = new List<User>();
+
+    mockRepo.Setup(r => r.GetAll()).Returns(emptyUsers);
+
+    var controller = new AuthController(mockRepo.Object);
+
+    // Act
+    var result = controller.GetAll().Result; 
+    var okResult = result as OkObjectResult;
+
+    // Assert
+    Assert.IsNotNull(okResult);
+    Assert.AreEqual(200, okResult.StatusCode);
+
+    var returnedUsers = okResult.Value as IEnumerable<User>;
+    Assert.IsNotNull(returnedUsers);
+    Assert.AreEqual(0, returnedUsers.Count());
 }
     }
+}
