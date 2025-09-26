dbbbb07b879576814f0b934783dcbac277c6a36d
Mohammed El Alladi <moal@eaaa.dk>
2025-09-26 15:25:37 +0200

    add service

--- UsersWebApi_Module3/Program.cs
+++ UsersWebApi_Module3/Program.cs
@@ -1,10 +1,12 @@
 using Microsoft.EntityFrameworkCore;
 using UsersWebApi_Module3.Data;
+using UsersWebApi_Module3.Models;
+using static UsersWebApi_Module3.Controllers.AuthController;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<AppDbContext>(opt =>opt.UseInMemoryDatabase("UserDb"));
+builder.Services.AddScoped<IRepository<User>, UserRepository>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
